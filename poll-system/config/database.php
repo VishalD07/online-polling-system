82488fbe69bced738c8dc878b00231e596c885de
Vishal Dhawde <vishaldhawde69@gmail.com>
2026-01-30 12:18:25 +0530

    Fix SQLite database config for production

--- poll-system/config/database.php
+++ poll-system/config/database.php
@@ -32,16 +32,12 @@ return [
     'connections' => [
 
         'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => null,
-            'journal_mode' => null,
-            'synchronous' => null,
-            'transaction_mode' => 'DEFERRED',
-        ],
+    'driver' => 'sqlite',
+    'database' => database_path('database.sqlite'),
+    'prefix' => '',
+    'foreign_key_constraints' => true,
+],
+
 
         'mysql' => [
             'driver' => 'mysql',
